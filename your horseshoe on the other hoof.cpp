fca7b1dae89911a28f79c9562304a3532a5ae957
salma kandeel <68400035+Salmakandel20@users.noreply.github.com>
2022-12-18 18:56:07 +0200

    Update your horseshoe on the other hoof.cpp

--- your horseshoe on the other hoof.cpp
+++ your horseshoe on the other hoof.cpp
@@ -1,22 +1,15 @@
 #include <iostream>
-#include<algorithm>
-#include<vector>
+#include<set>
 using namespace std;
 
 int main(){
 	int x,count=4;
-	  vector<int> arr;
-	  vector<int>::iterator findItem;
+	  set<int> arr;
 	for(int i=0;i<4;i++){
 		cin>>x;
-		findItem=find(arr.begin(),arr.end(),x);
-		if(findItem==arr.end()) {
-		   arr.push_back(x);
-		   count-=1;
-
-		}
+		arr.insert(x);
+		
 		
 	}
-    cout<<count;
+    cout<<(4-arr.size());
 }
-
